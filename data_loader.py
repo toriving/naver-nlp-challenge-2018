c02796676f6a6c53e0c631eafdf9ac4cc5b75446
toriving <toriving@gmail.com>
2020-08-31 00:32:43 +0900

    Refactoring data load path

--- data_loader.py
+++ data_loader.py
@@ -12,7 +12,6 @@ def _read_data_file(file_path, train=True):
             sentence = [[], [], []]
         else:
             idx, ejeol, ner_tag = line.split("\t")
-            # idx는 0부터 시작하도록 
             sentence[0].append(int(idx))
             sentence[1].append(ejeol)
             if train:
@@ -24,16 +23,18 @@ def _read_data_file(file_path, train=True):
 
 def test_data_loader(root_path):
     # [ idx, ejeols, nemed_entitis ] each sentence
-    file_path = os.path.join(root_path, 'test', 'test_data')
+    file_path = os.path.join(root_path, 'test.txt')
 
     return _read_data_file(file_path, False)
 
 def data_loader(root_path):
     # [ idx, ejeols, nemed_entitis ] each sentence
-    file_path = os.path.join(root_path, 'train', 'train_data')
+    file_path = os.path.join(root_path, 'train.txt')
 
     return _read_data_file(file_path)
 
 if __name__ == "__main__":
-    sentences = data_loader("data")
+    sentences = data_loader("data_in")
+    print(sentences[0])
+    sentences = test_data_loader("data_in")
     print(sentences[0])
